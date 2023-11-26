ca31a0d615e572e59494b815b197342ef2758933
Steven Young <sastudillo6@gmail.com>
2023-11-25 21:36:38 -0500

    Installed backend dependencies and configured cors, dotenv variables.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+//DOTENV PORT configuration setup
+dotenv.config();
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('Banking main server app is working.');
 });
 
-const PORT = 3000;
+// Middleware for cors
+app.get(cors());
+app.use(express.json());
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
